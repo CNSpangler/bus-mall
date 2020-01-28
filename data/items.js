fda083035f39286cbdf8f85c944a85003187a059
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-27 21:15:03 -0800

    set up files, assets, data

---
+++ data/items.js
@@ -0,0 +1,145 @@
+const bag = {
+    id: 'bag', 
+    image: 'bag.jpg', 
+    name: 'R2-D2'
+};
+
+const banana = {
+    id: banana,
+    image: 'banana.jpg',
+    name: 'Banana Hammer'
+};
+
+const bathroom = {
+    id: 'bathroom',
+    image: 'bathroom.jpg',
+    name: 'iPad Definitely Doesn\'t Watch You Pee'
+};
+
+const boots = {
+    id: 'boots',
+    image: 'boots.jpg',
+    name: 'Boots of Shame'
+};
+
+const breakfast = {
+    id: 'breakfast',
+    image: 'breakfast.jpg',
+    name: 'Home 1950s Diner'
+};
+
+const bubblegum = {
+    id: 'bubblMeat No-Nosegum',
+    image: 'bubblMeat No-Nosegum.jpg',
+    name: 'Meat No-Nos'
+};
+
+const chair = {
+    id: 'chair',
+    image: 'chair.jpg',
+    name: 'Naughty Chair'
+};
+
+const cthulu = {
+    id: 'cthulu',
+    image: 'cthulu.jpg',
+    name: 'HAIL YOUR DARK OVERLORD'
+};
+
+// START HERE 
+const dogDuck = {
+    id: 'dog-duck',
+    image: 'dog-duck.jpg',
+    name: 'A Fowl Trick to Play On Your Dog'
+};
+
+const dragon = {
+    id: 'dragon',
+    image: 'dragon.jpg',
+    name: 'Just Like Mom Used to Make'
+};
+
+const pen = {
+    id: 'pen',
+    image: 'pen.jpg',
+    name: 'Silver-where? Anywhere!'
+};
+
+const petSweep = {
+    id: 'pet-sweep',
+    image: 'pet-sweep.jpg',
+    name: 'Your Dog Will Hate You Forever'
+};
+
+const scissors = {
+    id: 'scissors',
+    image: 'scissors.jpg',
+    name: 'Want to Eat Less Pizza? Ask Me How!'
+};
+
+const shark = {
+    id: 'shark',
+    image: 'shark.jpg',
+    name: 'Shark-Nap-Do'
+};
+
+const sweep = {
+    id: 'sweep',
+    image: 'sweep.jpg',
+    name: 'Your Kid Will Hate You Forever'
+};
+
+const tauntaun = {
+    id: 'tauntaun',
+    image: 'tauntaun.jpg',
+    name: 'What\'s the internal temperature of a tauntaun? Luke-warm!'
+};
+
+const unicorn = {
+    id: 'unicorn',
+    image: 'unicorn.jpg',
+    name: 'Taste the Rainbow!'
+};
+
+const usb = {
+    id: 'usb',
+    image: 'usb.gif',
+    name: 'Definitely not a sex thing.'
+};
+
+const waterCan = {
+    id: 'water-can',
+    image: 'water-can.jpg',
+    name: 'Shower yourself in frustration!'
+};
+
+const wineGlass = {
+    id: 'wine-glass',
+    image: 'wine-glass.jpg',
+    name: 'Makes your wine taste like regret!'
+};
+
+const items = [
+    bag,
+    banana,
+    bathroom,
+    boots,
+    breakfast,
+    bubblegum,
+    chair,
+    cthulu,
+    dogDuck,
+    dragon,
+    pen,
+    petSweep,
+    scissors,
+    shark,
+    sweep,
+    tauntaun,
+    unicorn,
+    usb,
+    waterCan,
+    wineGlass
+];
+
+export default items;
