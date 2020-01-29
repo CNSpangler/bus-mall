c47df45ff62a1e5f5f2cbd1c4b8365be8855a28c
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-29 09:58:03 -0800

    add chart.js to results page

--- results.js
+++
@@ -1,7 +0,0 @@
-import { populateItems } from './app.js';
-
-console.log(populateItems());
-
-const results = localStorage.getItem('votes');
-
-document.getElementById('results').textContent = JSON.stringify(results, 0, 2);
---
+++ results/results.js
@@ -0,0 +1,33 @@
+const results = localStorage.getItem('BusMall Results');
+const votes = [];
+const names = [];
+
+results.forEach = (item) => {
+    votes.push(item.votes);
+    names.push(item.id);
+};
+
+// document.getElementById('results-chart').textContent = JSON.stringify(results, 0, 2);
+
+const ctx = document.getElementById('results-chart').getContext('2d');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: names,
+        datasets: [{
+            label: '# of Votes',
+            data: votes,
+            backgroundColor: ['lightblue', 'blue', 'yellow', 'green', 'purple', 'orange']
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
