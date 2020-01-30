34b54007a0e724d366777000691218a7dbabc06b
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-29 20:58:31 -0800

    pics now work

--- data/itemArray.js
+++ data/itemArray.js
@@ -1,121 +1,121 @@
 export const originalItemsArray = [
     {
         id: 'bag', 
-        image: '/bus-mall/assets/bag.jpg', 
+        image: 'assets/bag.jpg', 
         name: 'Not the droid you\'re looking for.'
     },
 
     {
         id: 'banana',
-        image: '/bus-mall/assets/banana.jpg',
+        image: 'assets/banana.jpg',
         name: 'Banana De-Boner'
     },
 
     {
         id: 'bathroom',
-        image: '/bus-mall/assets/bathroom.jpg',
+        image: 'assets/bathroom.jpg',
         name: 'Steve Jobs Definitely Doesn\'t Watch You Pee'
     },
 
     {
         id: 'boots',
-        image: '/bus-mall/assets/boots.jpg',
+        image: 'assets/boots.jpg',
         name: 'Boots of Shame'
     },
 
     {
         id: 'breakfast',
-        image: '/bus-mall/assets/breakfast.jpg',
+        image: 'assets/breakfast.jpg',
         name: 'Home 1950s Diner'
     },
 
     {
         id: 'bubblegum',
-        image: '/bus-mall/assets/bubblegum.jpg',
+        image: 'assets/bubblegum.jpg',
         name: 'Meat No-Nos'
     },
 
     {
         id: 'chair',
-        image: '/bus-mall/assets/chair.jpg',
+        image: 'assets/chair.jpg',
         name: 'Naughty Chair'
     },
 
     {
         id: 'cthulu',
-        image: '/bus-mall/assets/cthulhu.jpg',
+        image: 'assets/cthulhu.jpg',
         name: 'HAIL YOUR DARK OVERLORD'
     },
 
     {
         id: 'dog-duck',
-        image: '/bus-mall/assets/dog-duck.jpg',
+        image: 'assets/dog-duck.jpg',
         name: 'A fowl trick to play on your dog'
     },
 
     {
         id: 'dragon',
-        image: '/bus-mall/assets/dragon.jpg',
+        image: 'assets/dragon.jpg',
         name: 'Just like mom used to make'
     },
 
     {
         id: 'pen',
-        image: '/bus-mall/assets/pen.jpg',
+        image: 'assets/pen.jpg',
         name: 'Silver-where? Anywhere!'
     },
 
     {
         id: 'pet-sweep',
-        image: '/bus-mall/assets/pet-sweep.jpg',
+        image: 'assets/pet-sweep.jpg',
         name: 'Your dog will hate you forever.'
     },
 
     {
         id: 'scissors',
-        image: '/bus-mall/assets/scissors.jpg',
+        image: 'assets/scissors.jpg',
         name: 'Want to eat less pizza? Why, though?'
     },
 
     {
         id: 'shark',
-        image: '/bus-mall/assets/shark.jpg',
+        image: 'assets/shark.jpg',
         name: 'Shark-nap-do'
     },
 
     {
         id: 'sweep',
-        image: '/bus-mall/assets/sweep.png',
+        image: 'assets/sweep.png',
         name: 'Your kid will hate you forever'
     },
 
     {
         id: 'tauntaun',
-        image: '/bus-mall/assets/tauntaun.jpg',
+        image: 'assets/tauntaun.jpg',
         name: 'What\'s the internal temperature of a tauntaun? Luke-warm!'
     },
 
     {
         id: 'unicorn',
-        image: '/bus-mall/assets/unicorn.jpg',
+        image: 'assets/unicorn.jpg',
         name: 'Taste the Rainbow!'
     },
 
     {
         id: 'usb',
-        image: '/bus-mall/assets/usb.gif',
+        image: 'assets/usb.gif',
         name: 'Definitely not a sex thing.'
     },
 
     {
         id: 'water-can',
-        image: '/bus-mall/assets/water-can.jpg',
+        image: 'assets/water-can.jpg',
         name: 'Shower yourself in frustration!'
     },
 
     {
         id: 'wine-glass',
-        image: '/bus-mall/assets/wine-glass.jpg',
+        image: 'assets/wine-glass.jpg',
         name: 'Makes your wine taste like regret!'
     },
 ];
